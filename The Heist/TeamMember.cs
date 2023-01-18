4e31cc682d0e5605cc3edf7849901b43869f96a3
Jeremy White <jcwhite414@gmail.com>
2023-01-17 19:35:09 -0600

    removed unused using statements

--- The Heist/TeamMember.cs
+++ The Heist/TeamMember.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace The_Heist;
+﻿namespace The_Heist;
 
 public class TeamMember
 {
     public string? Name { get; set; }
     public int SkillLevel { get; set; }
     public double CourageLevel { get; set;}
-
+   
     public override string ToString()
     {
         return $"Name: {Name}, skill level: {SkillLevel}, Courage Level: {CourageLevel}";
