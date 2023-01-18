c01444c1528f6b0ea1ccc13b4250e245818c8298
Jeremy White <jcwhite414@gmail.com>
2023-01-17 19:28:13 -0600

    added To String method, removed unnecessary brackets

--- The Heist/TeamMember.cs
+++ The Heist/TeamMember.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace The_Heist
+namespace The_Heist;
+
+public class TeamMember
 {
-    internal class TeamMember
+    public string? Name { get; set; }
+    public int SkillLevel { get; set; }
+    public double CourageLevel { get; set;}
+
+    public override string ToString()
     {
-        public string? Name { get; set; }
-        public int SkillLevel { get; set; }
-        public double CourageLevel { get; set;}
+        return $"Name: {Name}, skill level: {SkillLevel}, Courage Level: {CourageLevel}";
     }
 }
