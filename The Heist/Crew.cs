7ce34dd25ceabb2bf520f6a2d42b692228ab10c0
Jeremy White <jcwhite414@gmail.com>
2023-01-18 21:30:31 -0600

    Crew with CrewScore getter

---
+++ The Heist/Crew.cs
@@ -0,0 +1,31 @@
+﻿namespace The_Heist
+{
+    public class Crew
+    {
+        public List<TeamMember> Members { get; set; } = new List<TeamMember>();
+  
+        public int CrewScore { get => GetCrewScore();}
+    public void PrintMyCrew()
+        {
+            Console.WriteLine("Your team consists of: ");
+            foreach (var member in Members)
+            {
+                Console.WriteLine(member);
+            }
+        }
+        private int GetCrewScore()
+        {
+            int score = 0;
+         foreach (var member in Members) 
+        {
+                score += member.SkillLevel;
+        }
+         return score;
+        }
+        public void PrintCrewScore()
+        {
+            Console.WriteLine(GetCrewScore());  
+        }
+    }
+
+}
