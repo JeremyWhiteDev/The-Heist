6a63f784a28eac1a874f38ec0e9f54e798a9bcdc
Jeremy White <jcwhite414@gmail.com>
2023-01-19 20:27:07 -0600

    Added method for generating luck

--- The Heist/Bank.cs
+++ The Heist/Bank.cs
@@ -3,4 +3,13 @@
 public class Bank
 {
     public int? Difficulty { get; set; }
+
+    public int? Luck { get => GetRandomNumber();}
+
+    private int GetRandomNumber()
+    {
+        Random rnd = new Random();
+
+        return rnd.Next(-10, 10);
+    }
 }
