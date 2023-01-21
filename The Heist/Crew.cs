b5660be297da192f883c777b94e75fb9c1cd8b6b
Jeremy White <jcwhite414@gmail.com>
2023-01-21 11:32:45 -0600

    removed unneccessary statements, renamed teamMember to CrewMember

--- The Heist/Crew.cs
+++ The Heist/Crew.cs
@@ -1,31 +1,31 @@
-﻿namespace The_Heist
+﻿namespace The_Heist;
+
+public class Crew
 {
-    public class Crew
+    public List<CrewMember> Members { get; set; } = new List<CrewMember>();
+
+    public int CrewScore { get => GetCrewScore();}
+public void PrintMyCrew()
     {
-        public List<TeamMember> Members { get; set; } = new List<TeamMember>();
-  
-        public int CrewScore { get => GetCrewScore();}
-    public void PrintMyCrew()
+        Console.WriteLine("Your team consists of: ");
+        foreach (var member in Members)
         {
-            Console.WriteLine("Your team consists of: ");
-            foreach (var member in Members)
-            {
-                Console.WriteLine(member);
-            }
-        }
-        private int GetCrewScore()
-        {
-            int score = 0;
-         foreach (var member in Members) 
-        {
-                score += member.SkillLevel;
-        }
-         return score;
-        }
-        public void PrintCrewScore()
-        {
-            Console.WriteLine(GetCrewScore());  
+            Console.WriteLine(member);
         }
     }
+    private int GetCrewScore()
+    {
+        int score = 0;
+     foreach (var member in Members) 
+    {
+            score += member.SkillLevel;
+    }
+     return score;
+    }
+    public void PrintCrewScore()
+    {
+        Console.WriteLine(GetCrewScore());  
+    }
 
+    
 }
